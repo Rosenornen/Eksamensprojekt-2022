d4ec0a2411f852e49aff882356a230ecbdde1385
Rosenornen <70527031+Rosenornen@users.noreply.github.com>
2022-12-12 12:29:25 +0100

    Man kan se andres opslag

--- screen/HentMad.js
+++ screen/HentMad.js
@@ -1,50 +1,77 @@
+import * as React from 'react';
+import {View, Text, FlatList, TouchableOpacity, StyleSheet} from 'react-native';
+import firebase from 'firebase';
+import {useEffect, useState} from "react";
 
-import {StyleSheet, Text, View, Button} from 'react-native'
-import React from 'react'
-import Baggrund from "../assets/Baggrundmindre.png"
-import styled from 'styled-components/native'
-import { StatusBar } from 'expo-status-bar';
-import { useNavigation } from '@react-navigation/native'
-import { auth } from '../firebase';
-import { db } from '../firebase';
-import { TouchableOpacity } from 'react-native-gesture-handler';
+const FoodList = ({navigation}) => {
 
+    const [foods,setFoods] = useState()
 
+    useEffect(() => {
+        if(!foods) {
+            firebase
+                .database()
+                .ref('/Food/')
+                .on('value', snapshot => {
+                    setFoods(snapshot.val())
+                });
+        }
+    },[]);
 
-export default function Home() {
+    // Vi viser ingenting hvis der ikke er data
+    if (!foods) {
+        return <Text>Loading...</Text>;
+    }
 
-    const navigation = useNavigation()
+    const handleSelectFood = id => {
+        /*Her søger vi direkte i vores array af foods og finder food objektet som matcher idet vi har tilsendt*/
+        const food = Object.entries(foods).find( food => food[0] === id /*id*/)
+        navigation.navigate('Food Details', { food });
+    };
 
-  /* Liiidt for simple logud, men det var nødvendigt for at teste */
+    // Flatlist forventer et array. Derfor tager vi alle values fra vores foods objekt, og bruger som array til listen
+    const foodArray = Object.values(foods);
+    const foodKeys = Object.keys(foods);
+
     return (
-      <>
-      <StatusBar style ="light"/>
-         <ImageBackground source={Baggrund} resizeMode="stretch"></ImageBackground>
-         <View>
-          <Text>DET HER ER HENT MAD</Text>
-         </View>
-        </>
-    )
-  }
-  
-  const ImageBackground = styled.ImageBackground`
-      flex: 1;
-      height: 100%
-  `
-  
-  const styles = StyleSheet.create({
-      Text:{
-        alignContent: "center",
-        justifyContent: "center",
-        alignSelf: "center"
-      },
-      logud:{
-        alignItems: 'center',
-        justifyContent: "center",
-      },
-      userDetails:{
-        alignSelf: "center",
-        justifyContent: "center",
-      }
-  
-  })
+        <View style = {styles.container}>
+          <Text></Text>
+           <FlatList
+            data={foodArray}
+            // Vi bruger FoodKeys til at finde ID på det aktuelle food og returnerer dette som key, og giver det med som ID til FoodListItem
+            keyExtractor={(item, index) => foodKeys[index]}
+            renderItem={({ item, index }) => {
+                return(
+                    <TouchableOpacity style={styles.foodItem} onPress={() => handleSelectFood(foodKeys[index])}>
+                        <Text>
+                             {item.Hvem}   {item.Hvor}   {item.Hvad}   {item.Id_}
+                        </Text>
+                    </TouchableOpacity>
+                )
+            }}
+        />
+        </View>
+       
+    );
+}
+
+export default FoodList;
+
+
+const styles = StyleSheet.create({
+    container: {
+      justifyContent: 'center',
+      marginTop: 35,
+      padding: 5
+    },
+    foodItem: {
+        flex: 1,
+        borderWidth: 1,
+        borderRadius:10,
+        margin: 5,
+        padding: 5,
+        height: 50,
+        justifyContent:'center'
+    },
+    label: { fontWeight: 'bold' },
+});
