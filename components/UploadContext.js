9b9126b74e750565f577114aa7663e1eb1c9f2c6
SorenHoltorp <s-h-g@live.dk>
2022-12-20 19:27:20 +0100

    lavet upload funktion

--- components/UploadContext.js
+++ components/UploadContext.js
@@ -4,19 +4,27 @@ import * as ImagePicker from "expo-image-picker";
 import { auth, db, firebase } from "../firebase"
 
 function UploadContext() {
-  const [text, setText] = useState('');
-  const [image, setImage] = useState(null);
+    const [hvem, setHvem] = useState('');
+    const [hvor, setHvor] = useState('');
+    const [hvad, setHvad] = useState('');
+    const [afhentningstidspunkt, setAfhentningstidspunkt] = useState('');
+    const [madtype, setMadtype] = useState('');
+    const [image, setImage] = useState(null);
 
   function uploadData() {
-    if (!text || !image) {
+    if (!hvem || !hvor || !hvad || !afhentningstidspunkt || !madtype || !image ) {
       // Display an error if the text or image is not set
       return;
     }
 
     // Write the data to the Firebase Realtime Database
     firebase.database().ref('/users/' + auth.currentUser?.uid).set({
-      text,
-      image
+    hvem,
+    hvor,
+    hvad,
+    afhentningstidspunkt,
+    madtype,
+    image
     });
   }
 
@@ -46,8 +54,33 @@ function UploadContext() {
     <View style={styles.container}>
       <TextInput
         style={styles.input}
-        value={text}
-        onChangeText={setText}
+        value={hvem}
+        placeholder="Hvem"
+        onChangeText={setHvem}
+      />
+      <TextInput
+        style={styles.input}
+        value={hvor}
+        placeholder="Hvor"
+        onChangeText={setHvor}
+      />
+      <TextInput
+        style={styles.input}
+        value={hvad}
+        placeholder="Hvad"
+        onChangeText={setHvad}
+      />
+      <TextInput
+        style={styles.input}
+        value={afhentningstidspunkt}
+        placeholder="Hvornår"
+        onChangeText={setAfhentningstidspunkt}
+      />
+      <TextInput
+        style={styles.input}
+        value={madtype}
+        placeholder="Madtype"
+        onChangeText={setMadtype}
       />
       <Button title="Select Image" onPress={selectImage} />
       {image && (
@@ -65,8 +98,8 @@ const styles = StyleSheet.create({
     justifyContent: 'center'
   },
   input: {
-    width: '80%',
-    height: 40,
+    width: 100,
+    height: 25,
     borderColor: 'gray',
     borderWidth: 1,
     margin: 10
