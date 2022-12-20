d2cd14f0e35af04638d4d4b927ab0e940997d87b
SorenHoltorp <s-h-g@live.dk>
2022-12-20 20:04:01 +0100

    Lavet madeltaljer om

--- screen/MadDetaljer.js
+++ screen/MadDetaljer.js
@@ -1,75 +1,72 @@
-import * as React from 'react';
-import { View, Text, Platform, FlatList, StyleSheet, Button, Alert } from 'react-native';
-import firebase from 'firebase';
-import {useEffect, useState} from "react";
+import React, { useState, useEffect } from 'react';
+import { View, Text, Image, StyleSheet } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
+import { auth, db, firebase } from "../firebase"
 
-const MadDetaljer = ({route,navigation}) => {
-    const [food,setFood] = useState({});
+function DisplayContext() {
+  const [hvem, setHvem] = useState('');
+  const [hvor, setHvor] = useState('');
+  const [hvad, setHvad] = useState('');
+  const [afhentningstidspunkt, setAfhentningstidspunkt] = useState('');
+  const [madtype, setMadtype] = useState('');
+  const [image, setImage] = useState(null);
 
-    useEffect(() => {
-        /*Henter food values og sætter dem*/
-        setFood(route.params.food[1]);
+  useEffect(() => {
+    fetchData();
+  }, []);
 
-        /*Når vi forlader screen, tøm object*/
-        return () => {
-            setFood({})
-        }
+  const fetchData = () => {
+    // Fetch the data for the current user
+    firebase.database().ref('/MadTilAfhentning/' + auth.currentUser?.uid).once('value', snapshot => {
+      const data = snapshot.val();
+
+      // Set the state with the fetched data
+      setHvem(data.hvem);
+      setHvor(data.hvor);
+      setHvad(data.hvad);
+      setAfhentningstidspunkt(data.afhentningstidspunkt);
+      setMadtype(data.madtype);
+      setImage(data.image);
     });
+  }
 
-
-    if (!food) {
-        return <Text>No data</Text>;
-    }
-
-    //all content
-    return (
-        <LinearGradient
+  return (
+    <LinearGradient
         colors={['#fce24e', 'white']}
         style={styles.LinearGradient}
         start={{ x: 1, y: 0 }}
         end={{ x: 1, y: 1 }}>
-       <View style={styles.container}>
-            {
-                Object.entries(food).map((item,index)=>{
-                    return(
-                        <View style={styles.row} key={index}>
-                            {/*Vores food keys navn*/}
-                            <Text style={styles.label}>{item[0]} </Text>
-                            {/*Vores food values navne */}
-                            <Text style={styles.value}>{item[1]}</Text>
-                        </View>
-                    )
-                })
-            }
-        </View>
-        </LinearGradient>
-    );
+        <View style={styles.container}>
+      <Text>Hvem: {hvem}</Text>
+      <Text>Hvor: {hvor}</Text>
+      <Text>Hvad: {hvad}</Text>
+      <Text>Afhentningstidspunkt: {afhentningstidspunkt}</Text>
+      <Text>Madtype: {madtype}</Text>
+      {image && (
+        <Image source={{ uri: image }} style={styles.image} />
+      )}
+    </View>
+    </LinearGradient>
+  );
 }
 
-export default MadDetaljer;
-
 const styles = StyleSheet.create({
-    LinearGradient: {
-        flex: 1,
-        justifyContent: 'center',
-        padding: 10
-      },
-    container: { 
-      flex: 1, 
-      justifyContent: 'flex-start',
-      marginTop: 120
-     },
-    row: {
-        margin: 5,
-        padding: 5,
-        flexDirection: 'row',
-    },
-    label: { 
-      width: 100, 
-      fontWeight: 'bold'
-    },
-    value: { 
-      flex: 1 
-    },
+  LinearGradient: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },  
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  image: {
+    width: 200,
+    height: 200,
+    margin: 10
+  },
+
 });
+
+export default DisplayContext;
