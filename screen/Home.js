73f5698be106070d663b5f4e17d579cab05de638
SorenHoltorp <s-h-g@live.dk>
2022-12-21 17:53:38 +0100

    home

--- screen/Home.js
+++ screen/Home.js
@@ -2,30 +2,8 @@ import {StyleSheet, Text, View, Button} from 'react-native'
 import React, { useState, useEffect } from 'react';
 import { firebase } from '../firebase';
 import { LinearGradient } from 'expo-linear-gradient';
-import { useNavigation } from '@react-navigation/native'
 
 export default function Home() {  
-  const [users, setUsers] = useState([]);
-  const [fullName, setFullName] = useState('');
-
-  useEffect(() => {
-    // Make sure the user is authenticated
-    if (firebase.auth.currentUser) {
-      // Get the current user's ID
-      const userId = firebase.auth.currentUser.uid;
-
-      // Use the user's ID to retrieve the user's full name from the database
-      const userRef = firebase.database().ref(`User/${userId}`);
-      userRef.on('value', snapshot => {
-        const user = snapshot.val();
-        // Make sure the user object is not null
-        if (user) {
-          setFullName(user.fullName);
-          console.log(userId); // print the value of fullName to the console
-        }
-      });
-    }
-  }, []);
 
   return (
     <LinearGradient
@@ -34,7 +12,7 @@ export default function Home() {
     start={{ x: 1, y: 0 }}
     end={{ x: 1, y: 1 }}>
       <View style={styles.container}>
-        <Text style={styles.overskrift}>VELKOMMEN TILBAGE {userId} </Text>
+        <Text style={styles.overskrift}>DASHBOARD</Text>
       </View>
     </LinearGradient>
   )
