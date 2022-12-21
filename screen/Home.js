484d72e256870d75d25e96b4c21ea758db45577b
SorenHoltorp <s-h-g@live.dk>
2022-12-21 21:33:10 +0100

    Home

--- screen/Home.js
+++ screen/Home.js
@@ -3,7 +3,18 @@ import React, { useState, useEffect } from 'react';
 import { firebase } from '../firebase';
 import { LinearGradient } from 'expo-linear-gradient';
 
-export default function Home() {  
+export default function Home() {
+  const [fullName, setFullName] = useState('');
+
+  useEffect(() => {
+    const unsubscribe = firebase.database().ref('/User/' + firebase.auth().currentUser.uid)
+      .on('value', snapshot => {
+        setFullName(snapshot.val().fullName);
+      });
+    return () => {
+      unsubscribe();
+    };
+  }, []);
 
   return (
     <LinearGradient
@@ -12,7 +23,7 @@ export default function Home() {
     start={{ x: 1, y: 0 }}
     end={{ x: 1, y: 1 }}>
       <View style={styles.container}>
-        <Text style={styles.overskrift}>DASHBOARD</Text>
+      <Text style={styles.label}>Current user:</Text> <Text style={styles.fullName}>{fullName}</Text>
       </View>
     </LinearGradient>
   )
@@ -29,5 +40,14 @@ const styles = StyleSheet.create({
   },
   overskrift:{
     top: "20%",
-  }
+  },
+  label: {
+    fontSize: 16,
+    color: '#333',
+  },
+  fullName: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: '#333',
+  },
 })
